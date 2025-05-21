61059024fca90cc0f45a6ecfcd16a7ad4d12ef5d
Orkuma Ivo <orkumaivo@Orkumas-MBP-2.broadband>
2025-05-21 12:44:04 +0100

    Add RecipeLayout and RecipeContainer and SearchContainer components for recipe pages

---
+++ src/app/recipes/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { RecipeContainer } from "@/components/recipe-container";
+
+export default function Page() {
+  return <RecipeContainer />;
+}
---
+++ src/components/search-container.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export function SearchContainer() {
+  return (
+    <div>
+      <Link href="recipes/ddddd">Search Container</Link>
+    </div>
+  );
+}
